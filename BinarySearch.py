d14017a206719123b4a79cf1e4d1d7ad5818ff65
JRodriguez-Repo <97987743+JRodriguez-Repo@users.noreply.github.com>
2023-09-17 19:31:30 -0400

    Added recursive binary search function

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,4 +1,9 @@
-def binarySearch(sequence, item):
+
+sequence_A = [-100000, -10000, -1000, -100, -10, 0, 10, 100, 1000, 10000, 100000, 1000000, 100000000000, 100000000000000]
+item_A = 0
+item_B = 100000
+
+def iterativeBinarySearch(sequence, item):
     startPoint = 0
     endPoint = len(sequence) - 1
 
@@ -17,7 +22,20 @@ def binarySearch(sequence, item):
             
     return None
 
-sequence_A = [-100000, -10000, -1000, -100, -10, 0, 10, 100, 1000, 10000, 100000, 1000000, 100000000000, 100000000000000]
-item_A = 0
 
-print(binarySearch(sequence_A, item_A))
+def recursiveBinarySearch(sequence, startpoint, endpoint, item):
+
+    if startpoint <= endpoint:
+        midpoint = (startpoint + endpoint) // 2
+
+        if sequence[midpoint] == item:
+            return midpoint
+        elif item < sequence[midpoint]:
+            return recursiveBinarySearch(sequence, startpoint, midpoint-1, item)
+        else:
+            return recursiveBinarySearch(sequence, midpoint+1, endpoint, item)
+    return None
+
+
+print(iterativeBinarySearch(sequence_A, item_A))
+print(recursiveBinarySearch(sequence_A, 0, len(sequence_A) -1, item_B))
