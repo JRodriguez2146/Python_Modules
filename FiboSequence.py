29dd5dcd71098ab3e53ccf04c08f8657fed69568
JRodriguez-Repo <97987743+JRodriguez-Repo@users.noreply.github.com>
2023-09-25 21:55:08 -0400

    Second function added

--- FiboSequence.py
+++ FiboSequence.py
@@ -1,5 +1,5 @@
 def fiboSequence():
-    n = int(input("Enter a number."))
+    n = int(input("Enter the amount of numbers in the sequence: "))
 
     num1 = 0
     num2 = 1
@@ -10,5 +10,23 @@ def fiboSequence():
         num2 = temp + num2
 
 
+def getNthFiboNum():
+    n = int(input("Enter a number to find in the Fibonacci sequence: "))
+
+    a = 0
+    b = 1
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        print('Your number is: {}'.format(a))
+    elif n == 1:
+        print('Your number is: {}'.format(b))
+    else:
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+        print('Number {} in the sequence is: {}'.format(n,b))
 
 fiboSequence()
+getNthFiboNum()
